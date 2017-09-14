5f35816cc546a97c40d6a249fdfd80c8caaf92ac
BatyaGG <b.saduanov@gmail.com>
2017-09-14 20:44:14 +0600

    Added figures

--- Example1.py
+++ Example1.py
@@ -5,29 +5,29 @@ import numpy as np
 if __name__ == "__main__":
     data = np.loadtxt("data.txt", delimiter=',')
     data = data[:, 0:2].T
-    gmm = GMM_GMR(4)
-    gmm.fit(data)
+    gmr = GMM_GMR(4)
+    gmr.fit(data)
     timeInput = np.linspace(1, np.max(data[0, :]), 100)
-    gmm.predict(timeInput)
+    gmr.predict(timeInput)
     fig = plt.figure()
 
     ax1 = fig.add_subplot(221)
     print type(ax1)
     plt.title("Data")
-    gmm.plot(ax=ax1, plotType="Data")
+    gmr.plot(ax=ax1, plotType="Data")
 
     ax2 = fig.add_subplot(222)
     plt.title("Gaussian States")
-    gmm.plot(ax=ax2, plotType="Clusters")
+    gmr.plot(ax=ax2, plotType="Clusters")
 
     ax3 = fig.add_subplot(223)
     plt.title("Regression")
-    gmm.plot(ax=ax3, plotType="Regression")
+    gmr.plot(ax=ax3, plotType="Regression")
 
     ax4 = fig.add_subplot(224)
     plt.title("Clusters + Regression")
-    gmm.plot(ax=ax4, plotType="Clusters")
-    gmm.plot(ax=ax4, plotType="Regression")
-    predictedMatrix = gmm.getPredictedMatrix()
+    gmr.plot(ax=ax4, plotType="Clusters")
+    gmr.plot(ax=ax4, plotType="Regression")
+    predictedMatrix = gmr.getPredictedMatrix()
     print predictedMatrix
     plt.show()
--- Example2.py
+++ Example2.py
@@ -5,52 +5,52 @@ import numpy as np
 if __name__ == "__main__":
     data = np.loadtxt("data2.txt", delimiter=',')
 
-    gmm = GMM_GMR(4)
-    gmm.fit(data)
+    gmr = GMM_GMR(4)
+    gmr.fit(data)
     timeInput = np.linspace(1, np.max(data[0, :]), 300)
-    gmm.predict(timeInput)
+    gmr.predict(timeInput)
 
     fig = plt.figure()
     fig.suptitle("Axis 1 vs axis 0")
 
     ax1 = fig.add_subplot(221)
     plt.title("Data")
-    gmm.plot(ax=ax1, plotType="Data")
+    gmr.plot(ax=ax1, plotType="Data")
 
     ax2 = fig.add_subplot(222)
     plt.title("Gaussian States")
-    gmm.plot(ax=ax2, plotType="Clusters")
+    gmr.plot(ax=ax2, plotType="Clusters")
 
     ax3 = fig.add_subplot(223)
     plt.title("Regression")
-    gmm.plot(ax=ax3, plotType="Regression")
+    gmr.plot(ax=ax3, plotType="Regression")
 
     ax4 = fig.add_subplot(224)
     plt.title("Clusters + Regression")
-    gmm.plot(ax=ax4, plotType="Clusters")
-    gmm.plot(ax=ax4, plotType="Regression")
+    gmr.plot(ax=ax4, plotType="Clusters")
+    gmr.plot(ax=ax4, plotType="Regression")
 
     fig = plt.figure()
     fig.suptitle("Axis 2 vs axis 1")
 
     ax1 = fig.add_subplot(221)
     plt.title("Data")
-    gmm.plot(ax=ax1, plotType="Data", xAxis=1, yAxis=2)
+    gmr.plot(ax=ax1, plotType="Data", xAxis=1, yAxis=2)
 
     ax2 = fig.add_subplot(222)
     plt.title("Gaussian States")
-    gmm.plot(ax=ax2, plotType="Clusters", xAxis=1, yAxis=2)
+    gmr.plot(ax=ax2, plotType="Clusters", xAxis=1, yAxis=2)
 
     ax3 = fig.add_subplot(223)
     plt.title("Regression")
-    gmm.plot(ax=ax3, plotType="Regression", xAxis=1, yAxis=2)
+    gmr.plot(ax=ax3, plotType="Regression", xAxis=1, yAxis=2)
 
     ax4 = fig.add_subplot(224)
     plt.title("Clusters + Regression")
-    gmm.plot(ax=ax4, plotType="Clusters", xAxis=1, yAxis=2)
-    gmm.plot(ax=ax4, plotType="Regression", xAxis=1, yAxis=2)
+    gmr.plot(ax=ax4, plotType="Clusters", xAxis=1, yAxis=2)
+    gmr.plot(ax=ax4, plotType="Regression", xAxis=1, yAxis=2)
 
-    predictedMatrix = gmm.getPredictedMatrix()
+    predictedMatrix = gmr.getPredictedMatrix()
     print predictedMatrix
 
     plt.show()
